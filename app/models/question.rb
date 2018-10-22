4e2019220dede1c27e605c891158b337c013a78d
jstricklin89 <jstricklin89@gmail.com>
2018-10-22 17:54:55 -0500

    backend steps complete

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,2 +1,5 @@
 class Question < ApplicationRecord
+  belongs_to :category 
+  has_many :answered_questions
+  has_many :users, through: :answered_questions
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,4 @@
 class User < ApplicationRecord
-end
+  has_many :answered_questions
+  has_many :questions, through: :answered_questions
+end 
