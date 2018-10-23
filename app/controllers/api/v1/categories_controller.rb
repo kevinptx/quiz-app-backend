de67f40e0bea3b9e435b38674cfe62edd7db909a
jstricklin89 <jstricklin89@gmail.com>
2018-10-23 17:29:50 -0500

    duplicate changes from code along with kevin

--- app/controllers/api/v1/categories_controller.rb
+++ app/controllers/api/v1/categories_controller.rb
@@ -1,2 +1,25 @@
 class Api::V1::CategoriesController < ApplicationController
+  def index
+    @categorys = Category.all
+    render json: @categorys
+  end
+
+  def update
+    @category.update(category_params)
+    if @category.save
+      render json: @category, status: :accepted
+    else
+      render json: { errors: @category.errors.full_messages }, status: :unprocessible_entity
+    end
+  end
+
+  private
+
+  def category_params
+    params.permit(:name)
+  end
+
+  def find_category
+    @category = Category.find(params[:id])
+  end
 end
