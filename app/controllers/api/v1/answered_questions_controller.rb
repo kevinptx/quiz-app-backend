0877e87c97a1c14baa5b70e7cf8b07dbcccf2432
kevinptx <kevin.peery@gmail.com>
2018-10-23 17:20:55 -0500

    controllers updated to display data

--- app/controllers/api/v1/answered_questions_controller.rb
+++ app/controllers/api/v1/answered_questions_controller.rb
@@ -1,2 +1,26 @@
 class Api::V1::AnsweredQuestionsController < ApplicationController
+  def index
+    @answeredquestions = AnsweredQuestion.all
+
+    render json: @answeredquestions
+  end
+
+  def update
+    @answeredquestion.update(answeredquestion_params)
+    if @answeredquestion.save
+      render json: @answeredquestion, status: :accepted
+    else
+      render json: {errors: @answeredquestion.errors.full_messages}, status: :unprocessible_entity
+    end
+  end
+
+  private
+
+  def answeredquestion_params
+    params.permit(:name)
+  end
+
+  def find_answeredquestion
+    @answeredquestion = AnsweredQuestion.find(params[:id])
+  end
 end
