3b7a6666f53713c04047fd12ef79f95aad503b64
jstricklin89 <jstricklin89@gmail.com>
2018-10-23 13:59:33 -0500

    added questions controller

--- app/controllers/api/v1/questions_controller.rb
+++ app/controllers/api/v1/questions_controller.rb
@@ -1,2 +1,25 @@
 class Api::V1::QuestionsController < ApplicationController
+  def index
+    @questions = Question.all
+    render json: @questions
+  end
+
+  def update
+    @question.update(question_params)
+    if @question.save
+      render json: @question, status: :accepted
+    else
+      render json: { errors: @question.errors.full_messages }, status: :unprocessible_entity
+    end
+  end
+
+  private
+
+  def question_params
+    params.permit(:name)
+  end
+
+  def find_question
+    @question = Question.find(params[:id])
+  end
 end
